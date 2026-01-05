03c4e007964604c57dd96f27b1d9016b939ffd51
Sougata Pal <palsougata256@gmail.com>
2026-01-05 20:24:11 +0530

    modified:   __pycache__/file_1.cpython-310.pyc 	modified:   file_1.py 	modified:   file_2.py

--- file_1.py
+++ file_1.py
@@ -1,4 +1,14 @@
-print(f'file_1 __name__: {__name__}')
+# print(f'file_1 __name__: {__name__}')
+
+# if __name__=="__main__":
+#     print('"__something__" :- is called "Dunder"')
+
+def add(x,y):
+    return x+y
 
 if __name__=="__main__":
-    print('"__something__" :- is called "Dunder"')
+    if add(1,2)==3:
+        print("Pass")
+    else:
+        print("Fail")
+        
--- file_2.py
+++ file_2.py
@@ -1,3 +1,5 @@
-print(f'file_2 __name__: {__name__}')
+# print(f'file_2 __name__: {__name__}')
 
 import file_1
+
+print(file_1.add(1,2))
