a88d4040bd7e06ac8253eba5454d9caff7bbb0fc
Sougata Pal <palsougata256@gmail.com>
2026-01-05 19:42:18 +0530

    decorators

--- py1.py
+++ py1.py
@@ -4,6 +4,8 @@
 
 import time
 
+#How to use decorators
+
 # def brew_tea():
 #     start_time=time.time()
 #     print("Brewing Tea...")
@@ -15,20 +17,77 @@ import time
 # if __name__=="__main__":
 #     brew_tea()
     
+# def timer_dec(base_fn):
+#     def enhanced_fn():
+#         start_time=time.time()
+#         base_fn()
+#         end_time=time.time()
+#         print(f"Task time={end_time-start_time} seconds")
+#     return enhanced_fn
+
+# def brew_tea():
+#     print("Brewing Tea...")
+#     time.sleep(1)
+#     print("Tea is ready.")
+    
+    
+# dec_brew_tea=timer_dec(brew_tea)
+# dec_brew_tea()
+        
+# def timer_dec(base_fn):
+#     def enhanced_fn():
+#         start_time=time.time()
+#         base_fn()
+#         end_time=time.time()
+#         print(f"Task time={end_time-start_time} seconds")
+#     return enhanced_fn
+
+# def brew_tea():
+#     print("Brewing Tea...")
+#     time.sleep(1)
+#     print("Tea is ready.")
+    
+    
+# brew_tea=timer_dec(brew_tea)
+# brew_tea()
+
+
+# def timer_dec(base_fn):
+#     def enhanced_fn():
+#         start_time=time.time()
+#         base_fn()
+#         end_time=time.time()
+#         print(f"Task time={end_time-start_time} seconds")
+#     return enhanced_fn
+
+# @timer_dec
+# def brew_tea():
+#     print("Brewing Tea...")
+#     time.sleep(1)
+#     print("Tea is ready.")
+  
+# brew_tea()
+
+
 def timer_dec(base_fn):
-    def enhanced_fn():
+    def enhanced_fn(*args):
         start_time=time.time()
-        base_fn()
+        base_fn(*args)
         end_time=time.time()
-        print(f"Task time={end_time-start_time} seconds")
+        print(f"Task time={end_time-start_time} seconds \n")
     return enhanced_fn
 
+@timer_dec
 def brew_tea():
     print("Brewing Tea...")
     time.sleep(1)
     print("Tea is ready.")
+ 
+@timer_dec
+def make_matcha(tea_type,steep_time):
+    print(f'Making {tea_type} matcha tea...')
+    time.sleep(steep_time)
+    print("Matcha is ready")
     
-    
-dec_brew_tea=timer_dec(brew_tea)
-dec_brew_tea()
-        
+make_matcha("Green",1)
+brew_tea()
