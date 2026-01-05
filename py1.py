b34928b8029fca2c2dd37d62f2459ee5555c3558
Sougata Pal <palsougata256@gmail.com>
2026-01-05 19:34:19 +0530

    added timer dec

--- py1.py
+++ py1.py
@@ -4,15 +4,31 @@
 
 import time
 
+# def brew_tea():
+#     start_time=time.time()
+#     print("Brewing Tea...")
+#     time.sleep(1)
+#     print("Tea is ready.")
+#     end_time=time.time()
+#     print(f"Task time={end_time-start_time} seconds")
+    
+# if __name__=="__main__":
+#     brew_tea()
+    
+def timer_dec(base_fn):
+    def enhanced_fn():
+        start_time=time.time()
+        base_fn()
+        end_time=time.time()
+        print(f"Task time={end_time-start_time} seconds")
+    return enhanced_fn
+
 def brew_tea():
-    start_time=time.time()
     print("Brewing Tea...")
     time.sleep(1)
     print("Tea is ready.")
-    end_time=time.time()
-    print(f"Task time={end_time-start_time} seconds")
-    
-if __name__=="__main__":
-    brew_tea()
     
     
+dec_brew_tea=timer_dec(brew_tea)
+dec_brew_tea()
+        
