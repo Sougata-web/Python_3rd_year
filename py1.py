e2bc81cb586c654e34b32010eb27b47b590dd4f6
Sougata Pal <palsougata256@gmail.com>
2026-01-05 19:29:39 +0530

    nothing

--- py1.py
+++ py1.py
@@ -1,3 +1,18 @@
 # First class for python
 
-print("Hello World")
+# print("Hello World")
+
+import time
+
+def brew_tea():
+    start_time=time.time()
+    print("Brewing Tea...")
+    time.sleep(1)
+    print("Tea is ready.")
+    end_time=time.time()
+    print(f"Task time={end_time-start_time} seconds")
+    
+if __name__=="__main__":
+    brew_tea()
+    
+    
