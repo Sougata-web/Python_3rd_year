bcb1a086ece7e63ac6cd5234990d1c52d9702438
Sougata Pal <palsougata256@gmail.com>
2026-01-06 19:51:47 +0530

    fstrings and lists

---
+++ py2.py
@@ -0,0 +1,37 @@
+# F-strings
+
+# f_temp=67
+
+# print(f'The tempeature is {(f_temp-32)/1.8:.2f} degrees Celcius')
+# print(f'The tempeature is {(f_temp-32)/1.8:.2e} degrees Celcius')
+# print(f'The tempeature is {(f_temp-32)/1.8:.2%} degrees Celcius')
+
+# print(f'The tempeature is {(f_temp-32)/1.8:,.2f} degrees Celcius')
+
+# f_temp_sun=27000000
+
+# print(f'The temperature of the sun is {(f_temp_sun-32)/1.8:,.2f} degrees Celcius')
+# print(f'The temperature of the sun is {(f_temp_sun-32)/1.8:15,.2f} degrees Celcius')
+# print(f'The temperature of the sun is {(f_temp_sun-32)/1.8:<15,.2f} degrees Celcius')
+
+# :[alignment][width],.[decimals][type]
+# print(f'The temperature of the sun is {(f_temp_sun-32)/1.8:^15,.2f} degrees Celcius')
+
+
+# Lists
+
+
+# friends=["Paul","Dana", "Nathan","Lilly"]
+
+# friends.append("Elina")
+# friends.remove("Dana")
+# # friends.sort()
+# print(friends)
+# print(len(friends))
+# print(friends[2])
+
+# [] :- List
+# () :- Tuple 
+# {} :- Set
+
+
