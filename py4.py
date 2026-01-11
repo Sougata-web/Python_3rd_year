786255765bb14a453e243ba20fe95c7948474f75
Sougata Pal <palsougata256@gmail.com>
2026-01-11 11:47:41 +0530

    new file:   data.txt 	modified:   py4.py

--- py4.py
+++ py4.py
@@ -22,5 +22,41 @@
 # result=", ".join(s)
 # print(result)
 
-squares={x: x*x for x in range(1,10)}
-print(squares)
+# squares={x: x*x for x in range(1,10)}
+# print(squares)
+
+# def my_decorator(func):
+#     def wrapper():
+#         print("Hello")
+#         func()
+#         print("GoodBye")
+#     return wrapper
+
+# @my_decorator
+# def say_hello():
+#     print("Sougata")
+    
+# say_hello()
+
+# class Character:
+#     def __init__(self, name,health,attack):
+#         self.name=name
+#         self.health=health
+#         self.attack=attack
+        
+#     def attack_enemy(self):
+#         print(f'{self.name} attacks with power {self.attack}')
+
+# warrior=Character("Thor",100,50)
+# mage=Character("Gandalf",80,70)
+# archer=Character('Archer',80,90)
+
+# warrior.attack_enemy()
+# mage.attack_enemy()
+# archer.attack_enemy()
+
+# with open('/Users/SOUGATA/OneDrive/Desktop/programs/3rd_year/Python/data.txt','a+') as file:
+#     content=input('Enter any content to store in a file: ')
+#     file.write(content+'\n')
+#     print('Saved!')
+
