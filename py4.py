84b2c3d10be7e57490d1202b17bab2bd6a5ccf76
Sougata Pal <palsougata256@gmail.com>
2026-01-11 11:13:55 +0530

    formating and joining

---
+++ py4.py
@@ -0,0 +1,9 @@
+# chai_type="Masala"
+# quantity=2
+# order="I ordered {} cups of {} chai."
+# print(order.format(quantity,chai_type))
+
+
+# chai_variety=["Lemon","Masala","Ginger"]
+
+# print(" ".join(chai_variety))
